c802d17d6a4d58018d41295439cfc265d79434ad
jurgob <jurgob@users.noreply.github.com>
2023-04-24 07:56:27 +0100

    tunnel with lt or ngcock

--- scripts/tunnel.js
+++ scripts/tunnel.js
@@ -1,22 +1,65 @@
 const localtunnel = require('localtunnel');
+const ngrok = require('ngrok');
 const fs = require('fs');
 
-(async () => {
+// type Service = "ngrok" | "localtunnel";
+
+async function localTunnel(){
   const tunnel = await localtunnel({ port: 3000 });
   const tunnelCapiFn = await localtunnel({ port: 3001 });
 
-const tunnelDomain = tunnel.clientId+".loca.lt"
-  console.log({
-    tunnel: tunnelDomain
-  })
+  const tunnelDomain = tunnel.clientId+".loca.lt"
   const tunnelDomainCapiFn = tunnelCapiFn.clientId+".loca.lt"
-  console.log({
-    tunnel: tunnelDomainCapiFn
-  })
-  fs.writeFileSync('.env.srv.local', tunnelDomain);
-  fs.writeFileSync('.env.capifn.local', tunnelDomainCapiFn);
 
-  tunnel.on('close', () => {
-    // tunnels are closed
-  });
+  return [
+    {domain: tunnelDomain,port:3000},
+    {domain: tunnelDomainCapiFn,port:3001}
+  ]
+}
+
+async function ngrokTunnel(){
+    
+    let url = await ngrok.connect({
+        port:3000,
+        region: 'eu',
+        bind_tls: true
+    });
+    url = url.split('://')[1]
+
+    let url2 = await ngrok.connect({
+        port:3001,
+        region: 'eu',
+        bind_tls: true
+    });
+    url2 = url2.split('://')[1]
+
+    console.log(url2)
+    return [
+        {domain: url,port:3000},
+        {domain: url2,port:3001}
+    ]
+}
+
+async function tunnelFunction(service){
+    if(["ngrok" , "localtunnel"].includes(service) === false) throw new Error("service not supported")
+    
+    if(service === "ngrok"){
+        return ngrokTunnel()
+    }else{
+        return localTunnel()
+    }
+}
+
+(async () => {
+  const service = "ngrok"
+  console.log(`statring tunnel with service ${service}`)
+  
+  const [t1, t2] = await tunnelFunction(service);
+
+  console.log(`${t1.port} ${t1.domain}`)
+  console.log(`${t2.port} ${t2.domain}`)
+
+  fs.writeFileSync('.env.srv.local', t1.domain);
+  fs.writeFileSync('.env.capifn.local', t2.domain);
+
 })();
